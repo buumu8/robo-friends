4409f3a5883d4756703cc1cfd2df5292b6d1a653
buumu8 <buumu8@gmail.com>
2022-09-20 17:29:10 +0700

    Updates

---
+++ precache-manifest.743857a15ded660db79ce99616a8898a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a41c0af0aa2ed22125",
+    "url": "/robo-friends/static/css/main.8c3d8afa.chunk.css"
+  },
+  {
+    "revision": "86a41c0af0aa2ed22125",
+    "url": "/robo-friends/static/js/main.ca716846.chunk.js"
+  },
+  {
+    "revision": "e7597b490344e6c76119",
+    "url": "/robo-friends/static/js/runtime~main.2d4732fc.js"
+  },
+  {
+    "revision": "052cde35c55d76bdc516",
+    "url": "/robo-friends/static/js/2.80d6cf23.chunk.js"
+  },
+  {
+    "revision": "b693c72f6e6033f7103c",
+    "url": "/robo-friends/static/js/3.c7d6837a.chunk.js"
+  },
+  {
+    "revision": "95a5d948de7e967c4adf6ff55fd0f1a2",
+    "url": "/robo-friends/index.html"
+  }
+];
